42c73a32bfe29178ab410f4dbed84bc5c79b64dc
tao159 <yangtao155@163.com>
2019-06-24 22:57:45 +0800

    登录注册接口测试，功能完善

--- pages/register.vue
+++ pages/register.vue
@@ -147,7 +147,7 @@ export default {
                     }).then(({status,data})=>{
                         if(status==200){
                             if(data&&data.code==0){
-                                // location.href="/login"
+                               location.href="/login"
                             }else{
                                 self.error=data.msg
                             }
